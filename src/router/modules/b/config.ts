fdaf6cec750e8377de643febd1b9ca1a5e1c493a
jfs <1244223469@qq.com>
2022-03-28 19:27:34 +0800

    fix:mibo

--- src/router/modules/b/config.ts
+++ src/router/modules/b/config.ts
@@ -3,6 +3,8 @@ import RouterView from '/src/App.vue';
 import RouterView from '/src/App.vue';
 import RouterView from '/src/App.vue';
 import RouterView from '/src/App.vue';
+import RouterView from '/src/App.vue';
+import RouterView from '/src/App.vue';
 //文件为插件产生 原则上只允许修改路由参数   
 export default {
     name: "b__config",
