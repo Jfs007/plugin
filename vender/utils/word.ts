681719b515a57e59c74774bfd00ee1793a01c8a6
jfs <1244223469@qq.com>
2022-04-02 20:01:53 +0800

    gaidong

--- vender/utils/word.ts
+++ vender/utils/word.ts
@@ -1,69 +1,94 @@
-export class Word  { 
+export class Word {
     constructor() {
 
     }
-    isObject(value:string):boolean {
+    isObject(value: string): boolean {
         return <boolean>(!!value.trim().match(/^\{.*\}$/))
     }
-    isArray(value:string):boolean {
+    isArray(value: string): boolean {
         return <boolean>(!!value.trim().match(/^\[.*\]$/))
     }
-    isNumber(value:string) :boolean {
+    isNumber(value: string): boolean {
         return <boolean>(!!value.trim().match(/^\d*$/));
     }
-    isString(value:string) :boolean {
+    isString(value: string): boolean {
         return <boolean>(!!value.trim().match(/^['|"].*['|"]$/));
     }
-    
-    isFunction(value:string):boolean {
+
+    isFunction(value: string): boolean {
         let f1 = <boolean>(!!value.match(/^function(.*).*{.*}$/));
         let f2 = <boolean>(!!value.match(/^(.*).*=>.*{.*}$/));
         return f1 || f2;
     }
-    typeof(value:string) {
-        if(this.isObject(value)) return 'object';
-        if(this.isArray(value)) return 'array';
-        if(this.isNumber(value)) return 'number';
-        if(this.isFunction(value)) return 'function';
-        if(this.isString(value)) return 'string';
-        if(value.trim() == 'undefined') return 'undefined';
-        if(value.trim() == 'null') return 'null';
+    typeof(value: string) {
+        if (this.isObject(value)) return 'object';
+        if (this.isArray(value)) return 'array';
+        if (this.isNumber(value)) return 'number';
+        if (this.isFunction(value)) return 'function';
+        if (this.isString(value)) return 'string';
+        if (value.trim() == 'undefined') return 'undefined';
+        if (value.trim() == 'null') return 'null';
         return 'var'
-        
+
+    }
+    scopeMerge(clapArray:Array<string>, ):Array<string> {
+
+        // clapArray.map()
+
+        return [];
     }
 
     analysis(code) {
         code = code.trim();
         let _obj = {};
-        let fileds_dirty = <Array<string>>(code.match(/([^\s.]*)\s{0,}:\s+(.*)/g) || []);
-        console.log(fileds_dirty, 'vvv');
-        // ['name: "home",', 'go: vv,']
-        let fileds = fileds_dirty.map((dirty:string): Array<string> => {
-            let clean:string =  dirty.replace(/,$/, '').trim();
-            let [exec, key, value] = <Array<string>>(clean.match(/(.*?):(.*)/) || []);
+        let clap_string:string = code.replace(/[\n\r]/g, '').match(/^\{(.*)\}$/)[1];
+        if(!clap_string) return _obj;
+        let keyValues = [];
+        let scopeIdentNum = { '{': 0, '[': 0, '}': 0, ']': 0 };
+       
+        clap_string.split(',').map(keyValue => {
+            
+            let scopeIdent = keyValue.match(/\[|\{|\}|\]/g) || [];
+            if ((scopeIdentNum['{'] == scopeIdentNum['}'] && scopeIdentNum['['] == scopeIdentNum[']'])) {
+                scopeIdentNum = { '{': 0, '[': 0, '}': 0, ']': 0 };
+                keyValues.push(keyValue);
+                
+            }else {
+                let lastKeyValue = keyValues[keyValues.length-1];
+
+                keyValues[keyValues.length-1]= lastKeyValue + ',' + keyValue;
+                
+            }  
+            scopeIdent.map(ident => {
+                scopeIdentNum[ident]++;
+            });
+
+        })
+        let fileds = keyValues.map((keyValue: string): Array<string> => {
+            let [exec, key, value] = <Array<string>>(keyValue.match(/(.*?):(.*)/) || []);
+           
+
             return [key.trim(), value.trim()];
         });
-        // console.log(fileds, 'fies');
 
         fileds.map(filed => {
             let [key, value] = filed;
-            if(this.isArray(value)) {
+            if (this.isArray(value)) {
+                console.log((value.trim()).match(/\[(.*)\]/)[1].split(','), '---match\n\n\n')
                 _obj[key] = (value.trim()).match(/\[(.*)\]/)[1].split(',').map(code => {
                     return this.analysis(code);
                 });
             }
-            else if(this.isObject(value)) {
+            else if (this.isObject(value)) {
                 _obj[key] = this.analysis(value);
-            }else {
+            } else {
                 _obj[key] = `"${value}"`;
             }
-
-            // _obj[key] 
         });
-        return _obj; 
+        return _obj;
     }
 
-    
+
     // let wordAnalysis = (code) => {
     //     code.match(/([^\s.]*)\s{0,}:\s+(.*)/g)
     // }
